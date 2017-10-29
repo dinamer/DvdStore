0de448940da17f79fa165c14507d42678f6542a2
Dina <Dina@DINA-PC>
2017-10-29 22:16:10 +0100

    Add project files.

---
+++ Edmiry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Edmiry.Startup))]
+namespace Edmiry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
